a962f6ff0427e2505edb6638c95644020e866143
GITZP <1356583208@qq.com>
2023-04-18 18:32:33 +0800

    v1.5

--- mushanimg-modules/mushanimg-module-system/mushanimg-system-start/src/main/java/com/hzb/system/SystemApplication.java
+++ mushanimg-modules/mushanimg-module-system/mushanimg-system-start/src/main/java/com/hzb/system/SystemApplication.java
@@ -1,5 +1,7 @@
 package com.hzb.system;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,8 +11,15 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @Date: 2023/4/17
  */
 @SpringBootApplication(scanBasePackages = {"com.hzb.system","com.alibaba.cola"})
+@Slf4j
 public class SystemApplication {
     public static void main(String[] args) {
+        log.info("Begin to start Spring Boot Application");
+        long startTime = System.currentTimeMillis();
+
         SpringApplication.run(SystemApplication.class, args);
+
+        long endTime = System.currentTimeMillis();
+        log.info("End starting Spring Boot Application, Time used: "+ (endTime - startTime) );
     }
 }
