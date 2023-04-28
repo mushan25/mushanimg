aac431f834f3c72912892ccfd22a3ab6628f004e
mushan2 <1356583208@qq.com>
2023-04-28 17:01:54 +0800

    v2.5

---
+++ mushanimg-modules/mushanimg-module-file/mushanimg-file-start/src/main/java/com/hzb/file/FileApplication.java
@@ -0,0 +1,15 @@
+package com.hzb.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: hzb
+ * @Date: 2023/4/28
+ */
+@SpringBootApplication(scanBasePackages = {"com.hzb.file", "com.alibaba.cola","com.hzb.base.core","com.hzb.base.security"})
+public class FileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+}
